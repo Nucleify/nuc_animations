e2b8a7ac776e7cfd0793d423c7f83f80e684cc3e
SzymCode <szymon.radomski@yahoo.com>
2026-01-31 21:32:19 +0100

    chore: remove old components

--- bounce/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './boune_fade_in'
-export * from './use_bounce_animation'
-export * from './use_bounce_click'
--- index.ts
+++ index.ts
@@ -6,10 +6,7 @@ export * from './nuc_animations'
 /**
  *  Folders exports
  */
-export * from './bounce'
 export * from './hexagons'
-export * from './scroll'
-export * from './split_text'
 
 /**
  *  File exports
